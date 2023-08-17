77ff10354465979cffb25c638ae2f6ece67a2a83
AvivPortal <avivportall@gmail.com>
2023-08-17 11:16:55 +0300

    Allproducts.ejs base working

--- Controllers/coffeeC.js
+++ Controllers/coffeeC.js
@@ -1,7 +1,16 @@
-const coffeeService = require('../services/coffeeService');
-const coffeeDB = require ('../Models/coffeeDB');
+const coffeeService = require('../services/coffeeS');
 
 const addCapsule = async (req,res) =>
 {
     const newCapsula = await coffeeService.addCapsule (req.body, res.json (newCapsula))
+}
+
+const getCapsules = async (req , res) =>{
+    const Capsules = await coffeeService.getAllCoffee()
+    res.render('allproducts',{capsules:Capsules})
+  }
+
+
+module.exports={
+    getCapsules
 }
--- Models/coffeeM.js
+++ Models/coffeeM.js
@@ -1,34 +1,37 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://seceret_coffee:secer_coffee@cluster0.333dgax.mongodb.net/shop?retryWrites=true&w=majority');
-
 const Schema = mongoose.Schema;
 
-const capsuleSchema = new mongoose.Schema({
-    Origin: {
-      type: String,
-      required: true,
+const capsule = new mongoose.Schema({
+    _id:String,
+
+    origin:{
+        type:String,
+        
     },
-      Name: {
-          type: String,
-           required: true,
-      },
-      Type: {// Arabica or Robusta
-          type: String,
-          required: true,
-      },
-      Intensity: { // 1-8
-          type: Number,
-          required: true,
-      },
-      Flavor: {
-          type: String,
-          required: true,
-      },
-      Price : {
-          type: Number,
-          require:true,
-      },
+
+    type:{
+        type:String,
+        
+    },
+
+    intensity:{
+        type:String,
+        
+    },
+
+    flavor:
+    {
+        type:String,
+    },
+
+    price:
+    {
+        type:String,
+    }
+
+
+
+
       });
 
-module.exports = mongoose.model('Capsule', capsuleSchema);
+module.exports = mongoose.model('products', capsule);
