f4794b8d43a5964964f0cd830cdb2878841ba710
csnotepad <soapierfriend@gmail.com>
2023-08-26 23:25:16 +0300

    editing code in users- controller+services

--- services/Users.js
+++ services/Users.js
@@ -5,29 +5,6 @@ async function login(username, password) {
     return user
 }
 
-
-//Working Register
-// async function register(username, password,firstName,lastName,Country,Adress,postalCode) {
-
-  
-
-//     const user = new User({
-//         _id: username,
-//         password:password,
-//         firstName:firstName,
-//         lastName:lastName,
-//         Country:Country,
-//         Adress:Adress,
-//         postalCode:postalCode,
-//         permission:0
-        
-//     });
-
-//     await user.save()        
-// }
-
-
-
 //New Register Method:
 async function register(email, password,firstName,lastName,Country,Address,postalCode) {
 try{
